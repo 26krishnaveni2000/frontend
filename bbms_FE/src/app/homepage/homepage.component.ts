98eb2654413886cada727324911f747f474e1752
veni <26krishnaveni2000@gmail.com>
2022-03-23 08:38:12 +0530

    Full homepage

--- bbms_FE/src/app/homepage/homepage.component.ts
+++ bbms_FE/src/app/homepage/homepage.component.ts
@@ -1,15 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, HostListener, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-homepage',
   templateUrl: './homepage.component.html',
   styleUrls: ['./homepage.component.css']
 })
+
 export class HomepageComponent implements OnInit {
 
+  
+  
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+  bgVariable:boolean = false;
+  headerVariable:boolean = false;
+  @HostListener('document:scroll')
+  onScroll(){
+    if(document.body.scrollTop > 60 || document.documentElement.scrollTop > 60) {
+      this.bgVariable = true;
+      this.headerVariable = true;
+    }
+    else{
+      this.bgVariable = false;
+      this.headerVariable = false;
+    }
   }
-
+  
 }
